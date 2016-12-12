f0a650418cd3caea333ab799c6b0536e47a12adf
Jason Lam <jasonlam604@gmail.com>
2016-12-11 18:55:17 -0800

    Init commit

---
+++ src/github.com/jasonlam604/gotools/cmd/gopath.go
@@ -0,0 +1,103 @@
+// Copyright © 2016 Jason Lam
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+    "path"
+    "strings"
+	"syscall"
+	"github.com/spf13/cobra"
+)
+
+// gopathCmd represents the gopath command
+var gopathCmd = &cobra.Command{
+	Use:   "gopath",
+	Short: "Updates $GOPATH and $GOPATH/bin to current working directory",
+	Long: `Updates $GOPATH and $GOPATH/bin to current working directory, 
+	       it will overwrite the existing $GOPATH and remove the existing
+	       $GOPATH/bin set in $PATH`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		loadPathInfo()
+
+		clearGoPathBin()
+
+		setGoPathAndBin()
+		
+		displayUpdates()
+
+		updateShell()
+	},
+}
+
+var goPathOrig string
+
+func init() {
+	RootCmd.AddCommand(gopathCmd)
+}
+
+func loadPathInfo() {
+	goPathOrig = os.Getenv("GOPATH")
+}
+
+func clearGoPathBin()  {
+	
+	if(len(goPathOrig) > 0) {
+		
+		var goBin = path.Join(goPathOrig,"bin");
+        
+        if strings.Index(os.Getenv("PATH"), goBin) > -1 {
+        	
+        	var path string = os.Getenv("PATH");
+        	
+        	path = strings.Replace(path, goBin, "", -1) 
+        	
+        	path = strings.Replace(path, "::", ":", -1)
+        	
+        	if strings.HasSuffix(path,":") {
+        		path = strings.TrimRight(path, ":")
+        	}
+        	
+        	if strings.HasPrefix(path,":") {
+        		path = strings.TrimLeft(path, ":")
+        	}
+        	
+        	os.Setenv("PATH", path)
+        } 
+	}
+}
+
+func setGoPathAndBin() {
+	
+	pwd, err := os.Getwd()
+    if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+    }
+
+	os.Setenv("GOPATH", pwd)
+	os.Setenv("PATH", os.Getenv("PATH") + ":" + path.Join(pwd,"bin"))	
+}
+
+func displayUpdates() {
+	fmt.Println("GOPATH: ", os.Getenv("GOPATH"))
+	fmt.Println("PATH: ", os.Getenv("PATH"))
+}
+
+func updateShell() {
+	syscall.Exec(os.Getenv("SHELL"), []string{os.Getenv("SHELL")}, syscall.Environ())
+}
---
+++ src/github.com/jasonlam604/gotools/cmd/myip.go
@@ -0,0 +1,80 @@
+// Copyright © 2016 Jason Lam
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+     "net"
+	"github.com/spf13/cobra"
+)
+
+// myipCmd represents the myip command
+var myipCmd = &cobra.Command{
+	Use:   "myip",
+	Short: "Finds your local IP, ignores loopback",
+	Long: `Finds your local IP, ignores loopback, not much more to say about this`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+        	ip, err := externalIP()
+        	if err != nil {
+              	  fmt.Println(err)
+        	}
+        	fmt.Println(ip)
+
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(myipCmd)
+}
+
+// Source from http://stackoverflow.com/questions/23558425/how-do-i-get-the-local-ip-address-in-go
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
+}
